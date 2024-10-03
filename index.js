7773d548f318d5d5af486fd2bddf17640d04c2f5
Paul Jubilee Owuoth <pauljubilee50@gmail.com>
2024-10-03 06:21:09 +0300

    completes lab

---
+++ index.js
@@ -0,0 +1,19 @@
+
+function receivesAFunction(callback) {
+    return callback(); 
+  }
+  
+ 
+  function returnsANamedFunction() {
+    return function namedFunction() {
+      return "I am a named function!";
+    };
+  }
+  
+ 
+  function returnsAnAnonymousFunction() {
+    return function() {
+      return "I am an anonymous function!";
+    };
+  }
+  
